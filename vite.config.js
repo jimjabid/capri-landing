3176b9903f0eec900955d11c810a93846ddf0c54
jabid jimenez <jabidandresjimenezserrano@gmail.com>
2025-05-22 19:30:59 -0300

    Update Vite configuration to include base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  base: '/capri-landing', // Replace with your actual repository name
 })
